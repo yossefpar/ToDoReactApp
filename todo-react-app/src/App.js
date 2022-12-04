6571651626b562db98c936ca851b18de328175ad
yossefpar <yossefpar@gmail.com>
2022-12-04 16:27:38 +0200

    refrasheWork

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,14 +1,25 @@
-import {React , useState , useRef} from "react";
+import React ,{ useState , useRef , useEffect} from "react";
 import TodoList from './TodoList';
+//import uuidv4 from 'uuid/dist/v4';
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'todoApp.todos'
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+
+  useEffect(()=> {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
   function hendelAddTodo(e) {
     const name = todoNameRef.current.value
     if(name === '') return
     setTodos(prevTodo => {
-      return [...prevTodo , {id: 2 , name: name , complete: false}]
+      return [...prevTodo , {id: 2/*uuidv4()*/, name: name , complete: false}]
     })
     todoNameRef.current.value = null
     
